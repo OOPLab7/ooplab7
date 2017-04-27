e2a912a32d4d4499ed197473d543aa55cecac21f
Quentin <quentin.garrigue@outlook.com>
2017-04-27 04:47:22 +0200

    SmartTV class

--- Lab7_201734021/src/package_common/SmartTV.java
+++ Lab7_201734021/src/package_common/SmartTV.java
@@ -1,5 +1,33 @@
 package package_common;
 
-public class SmartTV {
+public class SmartTV extends Product {
 
+	public boolean isConnected(){
+		
+		return true;
+	}
+	
+	public double maxSpeed(){
+		
+		double max= 10;
+		return max;
+	}
+	
+	public String getName(){
+		
+		String name="get name";
+		return name;
+	}
+	
+	public int getChannel(){
+		
+		int channel=23;
+		return channel;
+	}
+	
+	public void setChannel(){
+		
+		System.out.println("Channel");
+	}
+	
 }
