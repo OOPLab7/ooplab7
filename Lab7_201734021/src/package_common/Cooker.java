9b135526b5fb1f8434a46bb829009293964864f7
maxpolhaein <maxpolhaein@gmail.com>
2017-04-27 11:52:02 +0900

    Update Cooker.java

--- Lab7_201734021/src/package_common/Cooker.java
+++ Lab7_201734021/src/package_common/Cooker.java
@@ -2,10 +2,8 @@ package package_common;
 
 public class Cooker {
 	String food;
-	String name;
-	String Robin_test;
-	String Quentin_Test;
-	String Max_Test;
+	
+	
 	public String prepareFood(){
 		return food;
 		
