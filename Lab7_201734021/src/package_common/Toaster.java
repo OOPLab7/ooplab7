48ed1a0a7c007ecb7d51b8d75ff05326a6b17862
maxpolhaein <maxpolhaein@gmail.com>
2017-04-27 11:57:42 +0900

    Update Toaster.java

--- Lab7_201734021/src/package_common/Toaster.java
+++ Lab7_201734021/src/package_common/Toaster.java
@@ -1,18 +1,21 @@
 package package_common;
 
-public class Toaster {
+public class Toaster extends Cooker{
 	
 	String food;
-	
-	public void toaster(){
-		
-	}
-	public String prepareFood(){
-		return food;
-		
-	}
+	String name;
 	
 	public String getName() {
 		return name;
+	}
+	
+
+	public void toaster(){
+	
+	}
+	@Override
+	public String prepareFood(){
+		return food;
+		
 	}
 }
