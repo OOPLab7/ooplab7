924dbfa0cb94d4c524a862e189eb9703d54540ea
Quentin <quentin.garrigue@outlook.com>
2017-04-27 05:43:37 +0200

    Quentin modif

--- Lab7_201734021/src/package_common/ProductTest.java
+++ Lab7_201734021/src/package_common/ProductTest.java
@@ -3,25 +3,28 @@ package package_common;
 public class ProductTest {
 
 	
-	public void testNetworked(){
+	public void testNetworked(Networked NT){
 		
-		SmartTV ST = new SmartTV();
-		ST.isConnected();
-		ST.maxSpeed();
+		
+		System.out.println("isConnectedValue : "+NT.isConnected());
+		System.out.println("maxSpeedValue : "+NT.maxSpeed());
 	
 	}
 	
-	public void testCooker(){
+	public void testCooker(Cooker CK){
+	
 		
+		System.out.println("preparefoodValue"+CK.prepareFood());
 	}
 
 
 	
 	public static void main(String[] args) {
 		
+		Toaster TS = new Toaster("Bread","MaxPOL");
+		SmartTV TV = new SmartTV(23);
 		ProductTest pt = new ProductTest();	
 		
 		
-	
-
+	}
 }
