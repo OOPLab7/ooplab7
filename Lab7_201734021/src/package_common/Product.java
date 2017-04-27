42ddfbab623704b7a926ef49764231faf912b8d4
Max-Pol <Max-Pol@192.168.6.7>
2017-04-27 10:35:49 +0900

    ha

--- Lab7_201734021/src/package_common/Product.java
+++ Lab7_201734021/src/package_common/Product.java
@@ -1,5 +1,19 @@
 package package_common;
 
 public abstract class Product {
+	
+double price;
+String name;
 
+public double getPrice() {
+	return price;
+}
+
+public void setPrice(double price) {
+	this.price = price;
+}
+	
+public String getName(){
+	return name;
+}
 }
---
+++ Lab7_201734021/src/package_common/Toaster.java
@@ -0,0 +1,5 @@
+package package_common;
+
+public class Toaster {
+
+}
