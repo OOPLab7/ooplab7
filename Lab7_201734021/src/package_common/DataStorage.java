0e8aa9bf6efbb97dde69db1b9b552ff199f71670
Quentin <quentin.garrigue@outlook.com>
2017-04-27 04:32:03 +0200

    Data Storage Quentin

--- Lab7_201734021/src/package_common/DataStorage.java
+++ Lab7_201734021/src/package_common/DataStorage.java
@@ -2,4 +2,13 @@ package package_common;
 
 public class DataStorage {
 
+	public double getFreeCapacity(){
+		int a=3;
+		return a;
+	}
+	
+	public void format(){
+		
+		System.out.println("format");
+	}
 }
